5c18f6a493b98ead87b27f34520bedb8b057e3a6
Evan Ishibashi <evan.ishibashi@gmail.com>
2023-08-23 16:42:56 -0700

    added in functionality to populate questions

--- app.py
+++ app.py
@@ -3,7 +3,19 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 from stories import silly_story
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+
+@app.get('/questions')
+def populate_questions():
+
+    prompts = silly_story.prompts
+
+    return render_template("questions.html",
+                           prompt_names = prompts)
+
+
